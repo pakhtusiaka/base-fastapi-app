9912e5120369e92968d47e919b7954434723f412
pakhtusiaka <pahtusovmaks@yandex.ru>
2025-08-08 20:59:32 +0300

    init async alembic + config

--- alembic/env.py
+++ alembic/env.py
@@ -1,6 +1,8 @@
 import asyncio
 from logging.config import fileConfig
+from unittest.mock import Base
 
+from core.config import Settings
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
@@ -20,12 +22,13 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
+config.set_main_option("sqlalchemy.url", str(Settings.db.url))
 
 
 def run_migrations_offline() -> None:
