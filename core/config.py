be7d6f81aa0cec11c868ebd80191b65de27993cb
pakhtusiaka <pahtusovmaks@yandex.ru>
2025-08-08 20:53:12 +0300

    install alembic + black

--- core/config.py
+++ core/config.py
@@ -14,7 +14,7 @@ class ApiPrefix(BaseModel):
 
 class DatabaseConfig(BaseModel):
     url: PostgresDsn
-    echo: bool = False
+    echo: bool = True
     echo_pool: bool = False
     pool_size: int = 50
     max_overflow: int = 10
